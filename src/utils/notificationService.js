4fae52db0394608dda0785ebfae8e07181e45f8f
Rayhan <mohammadrayhan796@gmail.com>
2026-05-10 13:12:25 +0600

    feat: add notification service utility

---
+++ src/utils/notificationService.js
@@ -0,0 +1,6 @@
+// Notification service wrapper
+export const notificationService = {
+  success: (message) => console.log('Success:', message),
+  error: (message) => console.error('Error:', message),
+  info: (message) => console.info('Info:', message),
+};
