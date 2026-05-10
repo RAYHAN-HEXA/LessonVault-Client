47a0a1a680ea505b7dabafc3b1bfa81c7049a8e0
Rayhan <mohammadrayhan796@gmail.com>
2026-05-10 13:12:25 +0600

    feat: add localStorage utility helper

---
+++ src/utils/storage.js
@@ -0,0 +1,6 @@
+// LocalStorage helper functions
+export const storage = {
+  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  get: (key) => JSON.parse(localStorage.getItem(key)),
+  remove: (key) => localStorage.removeItem(key),
+};
