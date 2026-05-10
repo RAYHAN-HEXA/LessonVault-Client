a87d20db52a96e8f46fa512205834e215a24d3d7
Rayhan <mohammadrayhan796@gmail.com>
2026-05-10 13:13:20 +0600

    feat: add animation utility constants

---
+++ src/utils/animationUtils.js
@@ -0,0 +1,5 @@
+// Animation and transition helpers
+export const animationUtils = {
+  duration: { fast: 150, normal: 300, slow: 500 },
+  easing: { ease: 'ease', easeIn: 'ease-in', easeOut: 'ease-out' },
+};
