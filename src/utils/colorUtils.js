44be1ad046f08f81a7ac779648499cca88779256
Rayhan <mohammadrayhan796@gmail.com>
2026-05-10 13:13:20 +0600

    feat: add color utility functions

---
+++ src/utils/colorUtils.js
@@ -0,0 +1,4 @@
+// Color manipulation utilities
+export const colorUtils = {
+  hexToRgb: (hex) => ({ r: parseInt(hex.slice(1, 3), 16), g: parseInt(hex.slice(3, 5), 16), b: parseInt(hex.slice(5, 7), 16) }),
+};
