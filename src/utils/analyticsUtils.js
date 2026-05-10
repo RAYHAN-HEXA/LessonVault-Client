f2707332955c7d133b4becd5c6ceec03b379e8c5
Rayhan <mohammadrayhan796@gmail.com>
2026-05-10 13:13:34 +0600

    feat: add analytics utilities

---
+++ src/utils/analyticsUtils.js
@@ -0,0 +1,5 @@
+// Analytics tracking helpers
+export const analyticsUtils = {
+  trackEvent: (eventName, data) => console.log('Event tracked:', eventName),
+  trackPageView: (page) => console.log('Page view:', page),
+};
