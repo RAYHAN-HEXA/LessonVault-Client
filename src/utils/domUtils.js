87612760ca1c52384be234e2078d3439950392f2
Rayhan <mohammadrayhan796@gmail.com>
2026-05-10 13:13:20 +0600

    feat: add DOM utility functions

---
+++ src/utils/domUtils.js
@@ -0,0 +1,6 @@
+// DOM manipulation helpers
+export const domUtils = {
+  hasClass: (el, cls) => el.classList.contains(cls),
+  addClass: (el, cls) => el.classList.add(cls),
+  removeClass: (el, cls) => el.classList.remove(cls),
+};
